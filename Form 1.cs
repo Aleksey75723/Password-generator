0c4dc67ac9c935b7a91776bfe9ed99ba3256899e
JIexaSSD <JIexaSSD@JIEXASSD->
2022-10-07 00:04:41 +0500

    Добавьте файлы проекта.

---
+++ Form 1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_generator
+{
+    public partial class Form1 : Form
+    {
+        int listPosition;//для выделения текста
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+
+            numericUpDown1.Value = 10;
+            numericUpDown1.Minimum = 1;
+            numericUpDown1.Maximum = 15;
+        }
+
+    private void button1_Click(object sender, EventArgs e)
+        {
+            Randgen passGen = new Randgen();
+            richTextBox1.Text = passGen.Rand_gen((int)numericUpDown1.Value);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (richTextBox1.Find(Convert.ToString(i)) > 0)
+                {
+                    listPosition = richTextBox1.Find(Convert.ToString(i));
+                    richTextBox1.SelectionStart = listPosition;
+                    richTextBox1.SelectionLength = Convert.ToString(i).Length;
+                    //richTextBox1.SelectionColor = Color.Red;
+                    richTextBox1.SelectionFont = new Font("Times New Roman", 22, FontStyle.Bold);
+                }
+            }
+        }
+    }
+}
---
+++ Randgen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_generator
+{
+    //рандомно генерирует символы из списка
+    class Randgen
+    {
+        string password;
+        List<char> cList = new List<char>();
+        Random rnd = new Random();
+
+        public string Rand_gen(int count)
+        {
+            //список подходящих символов
+            cList.Add((char)33);
+            for (int i = 35; i <= 38; i++) { cList.Add((char)i); }
+            for (int i = 48; i <= 57; i++) { cList.Add((char)i); }
+            for (int i = 63; i <= 90; i++) { cList.Add((char)i); }
+            for (int i = 97; i <= 122; i++) { cList.Add((char)i);}
+
+            for (int i = 0; i < count; i++)
+            {
+                password += cList[rnd.Next(0, cList.Count)];
+            }
+            return password;
+        }
+    }
+}
